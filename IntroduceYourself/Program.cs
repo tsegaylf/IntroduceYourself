0d9a55f6d80d9fa237141dc088d3e5ddd958a955
Lidya Tsegay <lidyatsegay@outlook.com>
2019-08-27 09:19:40 -0400

    initial commit

---
+++ IntroduceYourself/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IntroduceYourself {
+    class Program {
+        static void Main(string[] args) {
+            string firstname = "Lidya";
+                string lastname = "Tsegay";
+            Console.WriteLine(firstname + " " + lastname);
+            string phone = "(513)999-9999";
+            Console.WriteLine(phone);
+            Console.WriteLine("lidya@maxtrain.com");
+            int streetnumber= 5390;
+            string address = "Lees Crossing Dr,"; 
+            Console.WriteLine(streetnumber + " " + address);
+            string city = "";
+            city = "Cincinnati,";
+            string state = "";
+            state = "OH";
+            int zip = 45239;
+            Console.WriteLine(city + " " + state + " " + zip);
+        }
+    }
+}
